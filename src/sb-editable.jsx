1f16027033cb7db5d9984900ec26a42e134dda46
devconn99 <devondeveloper99@gmail.com>
2019-10-24 14:07:52 -0300

    Merge pull request #7 from davidbailey00/add-some-features
    Misc fixes

---
+++ src/sb-editable.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class SbEditable extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    if (typeof this.props.content._editable === 'undefined' ||
+        window.location === window.parent.location) {
+      return
+    }
+
+    var el = ReactDOM.findDOMNode(this)
+    var options = JSON.parse(this.props.content._editable.replace(/^<!--#storyblok#/, '').replace(/-->$/, ''))
+
+    if (el instanceof Object && typeof el.setAttribute === 'function') {
+      el.setAttribute('data-blok-c', JSON.stringify(options))
+      el.setAttribute('data-blok-uid', options.id + '-' + options.uid)
+
+      this.addClass(el, 'storyblok__outline')
+    } else {
+      throw new TypeError('It seems that you are using a DOM text node inside the SbEditable wrapper. Please wrap it with an HTML DOM element.')
+    }
+  }
+
+  addClass(el, className) {
+    if (el.classList) {
+      el.classList.add(className)
+    } else if (!new RegExp('\\b'+ className+'\\b').test(el.className)) {
+      el.className += ' ' + className
+    }
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
+export default SbEditable
